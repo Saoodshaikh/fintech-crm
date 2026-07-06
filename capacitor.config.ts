62847fdedc17e1b251fa436c637da12935cc37f3
Saud Shaikh <saud.s@transvolt.in>
2026-07-06 10:09:21 +0530

    [added few modules]

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.finvest.crm',
+  appName: 'FinVest CRM',
+  webDir: 'dist'
+};
+
+export default config;
